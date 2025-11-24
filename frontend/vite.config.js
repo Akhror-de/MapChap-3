ade7bc638619034ff80feea64eecc2ff8555ddbe
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-11-24 17:08:26 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist',
-    sourcemap: false
-  },
-  server: {
-    host: true
+    outDir: 'dist'
   }
 })
