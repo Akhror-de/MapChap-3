b236b14ef67ddabc64e62116ced4d021ad5c9cfb
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-11-27 12:50:11 +0300

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// Импортируем Telegram стили
+import './styles/telegram.css'
+
 // Create app
 const app = createApp(App)
 
